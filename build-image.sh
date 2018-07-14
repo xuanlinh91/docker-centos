318962a5c318a6f60c1f7317f1987cfc212ed7bf
Nguyen Xuan Linh <xuanlinh91@gmail.com>
2018-07-14 20:15:36 +0900

    update script build image and try to fix php-fpm bug

--- build-image.sh
+++ build-image.sh
@@ -3,4 +3,4 @@
 echo "Start build centos, php, nginx, mariadb image!"
 docker build . -t centos-nginx -f centos-nginx
 docker build . -t centos-php -f centos-php
-docker build . -t centos-mariadb centos-mariadb
+docker build . -t centos-mariadb -f centos-mariadb
